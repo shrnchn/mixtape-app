b8e488b1bb4524cab22babcbefb54d240df2a79b
Sharon Chan <sharon.chan@telus.com>
2015-07-28 22:57:54 -0400

    fixed stylings and playlist name on click

--- js/scripts.js
+++ js/scripts.js
@@ -17,6 +17,7 @@ var drivingTime;
 var drivingTimeValue;
 
 var genre;
+var genreName;
 
 var videoId;
 var videoURL;
@@ -57,7 +58,7 @@ mixtape.codeAddress = function(){
 						} else {
 							alert('Broken!');
 						}
-
+					// fail alert
 					} else {
 						swal({   
 							title: "Error!",
@@ -142,13 +143,15 @@ mixtape.displayMap = function(){
 			var drivingTimeRounded = Math.round(drivingTimeValue);
 
 			// console.log(drivingTimeRounded);
-
+			// put text into divs
 			$('.from').text(orgAddress);
 			$('.to').text(destAddress);
 			$('.distance').text(distance);
 			$('.duration').text(drivingTime);
 
 			mixtape.estimateNumberSongs(drivingTimeRounded);
+
+		// if status of route is not okay, do fail alert
 		} else {
 			swal({   
 				title: "Error!",
@@ -156,7 +159,7 @@ mixtape.displayMap = function(){
 				type: "error",
 				confirmButtonText: "Got it!"
 			}, function(){
-				// reload window at top
+				// reload window at top when alert is done
 				location.reload();
 				window.onbeforeunload = function(){ window.scrollTo(0,0); }
 			});
@@ -165,6 +168,7 @@ mixtape.displayMap = function(){
 	});
 };
 
+// blue and green road lines
 mixtape.showRoute = function(){
 
 	// show route between origin and destination
@@ -189,7 +193,7 @@ mixtape.showRoute = function(){
 		title: "Destination"
 	});
 
-	// creates info windows for origin and destination
+	// creates info windows for origin and destination, set text inside
 	var orgInfo = 'Start: '+ orgAddress;
 	var destInfo = 'End: ' + destAddress;
 
@@ -216,18 +220,21 @@ mixtape.showRoute = function(){
 
 };
 
+// get music genre from input radio, only one choice can be selected
 mixtape.getMusicGenre = function(){
 
 	$('input[type="radio"]').on('click', function(){
 	// when button is clicked, store the genre in a variable
-		genre = $(this).data('genre');
-		$('.genre-name').text(genre);
+		genreName = $(this).data('genre');
 		// encode genres with multiple words
-		genre = encodeURIComponent(genre);
+		genre = encodeURIComponent(genreName);
 	});
 };
 
-// get number of songs
+// get number of songs, just an estimation..rounded up
+// avg of 4.5mins per song...estimating driving minutes/hrs divided per song
+// note: echonest api won't display pass 250 songs for results
+// todo: write this more efficiently
 mixtape.estimateNumberSongs = function(drivingtime){
 
 	var numberOfSongs;
@@ -236,49 +243,79 @@ mixtape.estimateNumberSongs = function(drivingtime){
 		numberOfSongs = 15;
 	} else if(drivingtime >= 60 && drivingtime <= 120) {
 		numberOfSongs = 30;
-	} else if(drivingtime >= 120 && drivingtime <= 240) {
-		numberOfSongs = 60;
-	} else if(drivingtime >= 240 && drivingtime <= 360) {
+	} else if(drivingtime >= 120 && drivingtime <= 180) {
+		numberOfSongs = 40;
+	} else if(drivingtime >= 180 && drivingtime <= 240) {
+		numberOfSongs = 55;
+	} else if(drivingtime >= 240 && drivingtime <= 300) {
+		numberOfSongs = 70;
+	} else if(drivingtime >= 300 && drivingtime <= 360) {
+		numberOfSongs = 80;
+	} else if(drivingtime >= 360 && drivingtime <= 420) {
 		numberOfSongs = 90;
-	} else if(drivingtime >= 360 && drivingtime <= 480) {
+	} else if(drivingtime >= 420 && drivingtime <= 480) {
+		numberOfSongs = 110;
+	} else if(drivingtime >= 480 && drivingtime <= 540) {
 		numberOfSongs = 120;
+	} else if(drivingtime >= 540 && drivingtime <= 600) {
+		numberOfSongs = 135;
+	} else if(drivingtime >= 600 && drivingtime <= 660) {
+		numberOfSongs = 150
+	} else if(drivingtime >= 660 && drivingtime <= 700) {
+		numberOfSongs = 160;
+	} else if(drivingtime >= 700 && drivingtime <= 760) {
+		numberOfSongs = 170;
+	} else if(drivingtime >= 760 && drivingtime <= 800) {
+		numberOfSongs = 180;
+	} else if(drivingtime >= 800 && drivingtime <= 860) {
+		numberOfSongs = 190;
+	} else if(drivingtime >= 860 && drivingtime <= 920) {
+		numberOfSongs = 210;
+	} else if(drivingtime >= 920 && drivingtime <= 980) {
+		numberOfSongs = 220;
+	} else if(drivingtime >= 980 && drivingtime <= 1040) {
+		numberOfSongs = 230;
 	} else {
 		numberOfSongs = 250;
 	}
 
+	// pass in estimation number of songs to create playlist..display that number
 	mixtape.createPlaylist(numberOfSongs);
 };
 
 mixtape.createPlaylist = function(numberOfSongs){
-
+	// ajax call to echonest api
 	$.ajax({
 		url: 'http://developer.echonest.com/api/v4/playlist/basic?api_key='+mixtape.ENkey+'&genre='+genre+'&format=json&results='+numberOfSongs+'&type=genre-radio',
 
 		type: 'GET',
 		dataType: 'json',
 		success: function(result){
+			// change playlist title
+			$('.genre-name').text(genreName);
+
 			// console.log(result);
 			mixtape.songs = result.response.songs;
-
+			// loop each song and display it as a list
 			$.each(result.response.songs,function(i,song){
+				// create a link with song title and artist name inside
 				var a = $('<a>').text(song.title + ' by ' + song.artist_name);
+				// append that link into the list item
 				var li = $('<li>').append(a);
 
 				$('.playlist ol').append(li);
 
+				// pass song title and artist name in list to get youtube videos in order
 				mixtape.getVideoId(song.title + song.artist_name, li);
 
 			});
-		},
-		error: function(err){
-			console.log(err);
 		}
 	});
 };
 
 // get video ID to pass into youtube URL
 mixtape.getVideoId = function(songName, li){
-
+	// ajax call to youtube api
 	$.ajax({
 		url: "https://www.googleapis.com/youtube/v3/search",
 		type: "GET",
@@ -290,6 +327,7 @@ mixtape.getVideoId = function(songName, li){
 			key: mixtape.YTkey
 		},
 		success: function(result){
+			// if video can't be found in object, append msg to list item
 			if(!result.items[0]) {
 				var noVidMsg = " - No video sorry!";
 
@@ -297,11 +335,13 @@ mixtape.getVideoId = function(songName, li){
 				return; // skip it! 
 			}
 
+			// store youtube vid id into variable, pass it into a string to create videoURL
 			videoId = result.items[0].id.videoId;
 			// console.log(videoId);
 
 			var videoURL = 'https://www.youtube.com/watch?v=' + videoId;
 			
+			// in list, find anchor, replace href with videoURL, add class for styling
 			$(li).find('a').attr({
 				'href' : videoURL,
 				target : '_blank',
@@ -313,11 +353,13 @@ mixtape.getVideoId = function(songName, li){
 
 mixtape.init = function(){
 
-	var getMusic = mixtape.getMusicGenre();
+	// get genre value
+	mixtape.getMusicGenre();
 
+	// when let's go button is clicked,
 	$('input[type="button"]').on('click', function(e){
 		e.preventDefault();
-
+		// check if genre is undefined, if so..do fail alert
 		if(genre === undefined) {
 			swal({   
 				title: "Error!",
@@ -325,12 +367,14 @@ mixtape.init = function(){
 				type: "error",
 				confirmButtonText: "Got it!"
 			});
+		// if not, code the routes on map and display the playlist with songs
 		} else {
 			$('.playlist ol').empty();
 			mixtape.codeAddress();
 		}
 	});
 
+	// if search again button is clicked, reload site to top of window
 	$('#reset').on('click', function() {
 	    // reload window at top
 	    location.reload();
@@ -338,6 +382,7 @@ mixtape.init = function(){
 	});
 };
 
+// doc ready, run this shiz!
 $(function(){
 	mixtape.init();
 });
